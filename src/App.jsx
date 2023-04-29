7a2c55043cfbb51ecd67ae0cadc2400265868d80
Nash0123 <2191091086@uttarauniversity.edu.bd>
2023-04-29 22:28:56 +0600

    Added Card Items

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from '../src/components/Header/Header';
+import Shop from '../src/components/Shop/Shop';
+import './App.css';
+
+function App () {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Shop></Shop>
+    </div>
+  );
+}
+
+export default App;
